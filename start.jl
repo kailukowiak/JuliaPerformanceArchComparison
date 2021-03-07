a224b133014e812c2d3b02b245ea512c420025cb
Kai Lukowiak <kailukowiak@gmail.com>
2021-03-07 13:33:03 -0700

    Made start also test it outside of ipython

--- start.jl
+++ start.jl
@@ -1,3 +1,16 @@
 ##
-using IJulia
-jupyterlab()
+using BenchmarkTools
+using Random
+##
+function mysum(x)
+    ans = 0
+    for i ∈ x
+        ans += i
+    end
+    return ans
+end
+##
+Random.seed!(42)
+x = rand(10^7)
+##
+@benchmark mysum(x)
